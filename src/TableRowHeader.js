18c2162be438b79ad29f4acb8adb1e3afe739f44
Jason Roundtree <jasontree123@gmail.com>
2019-03-28 21:40:39 -0700

    Rename this component

--- src/TableRowHeader.js
+++ src/TableRowHeader.js
@@ -2,12 +2,16 @@ import React from 'react'
 import plusSignIcon from './assets/001-add.svg'
 import minusSignIcon from './assets/002-minus.svg'
 
-export default function TableRowContainer(props) {
+export default function TableRowHeader(props) {
+  const toggleCollapseIcon = props.icon === true 
+    ? plusSignIcon 
+    : minusSignIcon
+
   return (
-    <tr id={props.date}>
+    <tr>
         <th onClick={() => props.toggleCollapsedDates(props.date)}>
         <img 
-            src={minusSignIcon} 
+            src={toggleCollapseIcon} 
             alt="Minus sign to collapse rows" 
         />
         </th>
